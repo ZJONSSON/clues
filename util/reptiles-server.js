10feca3fbba12d55e4b526674de319c73444239b
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2015-05-23 11:01:48 -0400

    Simplify reptiles server: No need to wrap the function.  A predetermined selection should now be placed in options

--- util/reptiles-server.js
+++ util/reptiles-server.js
@@ -13,53 +13,50 @@ function jsonReplacer(key, value) {
   return value;
 }
 
-module.exports = function(api,config) {
+function noop() {}
+
+module.exports = function(api,options) {
   api = api || {};
-  config = config || {};
-  
+  options = options || {};
+
   function stringifyError(e) {
     var err = {error: true};
     Object.getOwnPropertyNames(e)
       .forEach(function(key) {
         err[key] = e[key];
       });
-    if (e.stack && !config.debug) {
+    if (e.stack && !options.debug) {
       err.message = 'Internal Error';
       delete e.stack;
     }
-
     return err;
   }
 
-  function noop() {};
+  if (typeof(options.select) === 'string')
+  options.select = options.select.split(',');
 
-  return function(select,options) {
-    options = options || {};
-    if (typeof(select) === 'string')
-      select = select.split(',');
+  return function(req,res) {
+    var _res = (!options.quiet) ? res : {set: noop, write: noop, flush: noop},
+        pretty = req.query.pretty && 2,
+        first = '{                                     \t\n\n';
+    req.body = req.body || {};
+    _res.set('Transfer-Encoding','chunked');
+    _res.set('Content-Type', 'application/json; charset=UTF-8');
+    _res.set('Cache-Control', 'no-cache, no-store, max-age=0');
+    
+    if (typeof(res.flush) == 'function') _res.flush();
 
-    return function (req,res) {
-      var _res = (!options.quiet) ? res : {set: noop, write: noop, flush: noop},
-          pretty = req.query.pretty && 2,
-          first = '{                                     \t\n\n';
-      req.body = req.body || {};
-      _res.set('Transfer-Encoding','chunked');
-      _res.set('Content-Type', 'application/json; charset=UTF-8');
-      _res.set('Cache-Control', 'no-cache, no-store, max-age=0');
-      
-      if (typeof(res.flush) == 'function') _res.flush();
+    Object.keys(req.query || {})
+      .forEach(function(key) {
+        req.body[key] = req.query[key];
+      });
 
-      Object.keys(req.query || {})
-        .forEach(function(key) {
-          req.body[key] = req.query[key];
-        });
-
-      Object.keys(req.params || {})
-        .forEach(function(key) {
-          req.body[key] = req.params[key];
-        });
-      
-      var data = (select || req.params["fn"].split(','))
+    Object.keys(req.params || {})
+      .forEach(function(key) {
+        req.body[key] = req.params[key];
+      });
+    
+    var data = (options.select || req.params.fn.split(','))
       .map(function(ref) {
         ref = ref.replace(/\//g,'.');
         var facts = Object.create(api);
@@ -76,17 +73,16 @@ module.exports = function(api,config) {
           });
       });
 
-      req.on('close',function() {
-        data.forEach(function(d) {
-          d.cancel();
-        });
+    req.on('close',function() {
+      data.forEach(function(d) {
+        d.cancel();
       });
+    });
 
-      return Promise.all(data)
-        .then(function() {
-          _res.write('"__end__" : true\t\n}');
-          res.end();
-        });
-    };
+    return Promise.all(data)
+      .then(function() {
+        _res.write('"__end__" : true\t\n}');
+        res.end();
+      });
   };
 };
