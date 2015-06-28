7e8afc1ce0e93830807f2237e713f71b7c3c50bd
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2015-06-28 09:09:34 -0400

    Fix: Allow api to be an array-defined formula and ensure access to $global

--- util/reptiles-server.js
+++ util/reptiles-server.js
@@ -59,16 +59,17 @@ module.exports = function(api,options) {
         req.body[key] = req.params[key];
       });
 
-    var facts = (typeof(api) === 'function')
-          ? clues({},api,$global,'reptiles')
-          : Object.create(api);
-
     var $global = Object.create(options.$global || {},{
       res : {value: res},
       req : {value: req},
-      input: {value: req.body},
-      root : {value: facts}
+      input: {value: req.body}
     });
+
+    if (typeof(api) === 'object')
+      api = Object.create(api);
+
+    var facts = clues({},api,$global,'reptiles');
+    $global.root = facts;
     
     // The api request is either determined by options.select, req.param.fn or by remaining url
     var data = (options.select || decodeURI((req.params && req.params.fn) || req.url.slice(1).replace(/\//g,'.').replace(/\?.*/,'')).split(','))
