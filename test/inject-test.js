7116fdbf67ca9d646659d099b794a05afda74272
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2016-10-28 12:20:04 -0400

    Switch inject to lazy
    Before: we had to resolve the full base prior to injecting, even though the path injected never ends up being traversed by the user. Example: injecting 'a.b.c.d' would require solving `a.b.c` at the time of injection, before inserting `d`.
    
    After:  Injection is a recursive function down the base path that is not executed unless the path is traversed by the user.  Example: injecting `a.b.c.d` would only affect `a` initially, and then affect `b` when `a` is requested etc.

--- test/inject-test.js
+++ test/inject-test.js
@@ -14,7 +14,8 @@ var Logic = {
       userid: {value: userid}
     });
   },
-  userid : ['input.userid',String]
+  userid : ['input.userid',String],
+  never_traversed : {}
 };
 
 
@@ -25,7 +26,11 @@ var injected = function($global) {
     },
     'db.user' : function(userid) {
       return { desc: 'this is an injected response', userid: userid};
-    }
+    },
+    'impossible.to.reach' : 42,
+    'possible' : {},
+    'possible.value': 42,
+    'never_traversed.value' : 11
   },$global);
 };
 
@@ -34,6 +39,29 @@ describe('inject',function() {
     return clues(injected,'db.user',{input:{userid:'johndoe'}})
       .then(function(d) {
         assert.equal(d.userid,'test_johndoe');
+      });
+  });
+
+  it('allows sequential injections',function() {
+    return clues(injected,'possible.value',{})
+      .then(function(d) {
+        assert.equal(d,42);
+      });
+  });
+
+  it('ignores base paths that do not exists',function() {
+    return clues(injected,'impossible.to.reach',{})
+      .then(function() {
+        throw 'SHOULD_ERROR';
+      },function(e) {
+        assert.equal(e.message,'impossible not defined');
+      });
+  });
+
+  it('does not execute injected paths that are not traversed',function() {
+    return clues({},injected)
+      .then(function(injected) {
+        assert(typeof injected.never_traversed === 'function');
       });
   });
 });
