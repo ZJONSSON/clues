64050a8069e9037cb804bcb4442983367e61a8b6
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2017-08-09 13:17:52 -0400

    If the function we are solving is a class, then create a new instance by solving for the variables in the constructor

--- test/class-fn-test.js
+++ test/class-fn-test.js
@@ -2,9 +2,13 @@ const clues = require('../clues');
 const Promise = require('bluebird');
 const t = require('tap');
 
-t.test('Class functions', async t => {
+t.test('Class functions', {autoend: true}, async t => {
 
   class TestClass {
+    constructor(d,e) {
+      this.d = d;
+      this.e = e;
+    }
     method1(a, b) {
       return a * b;
     }
@@ -17,6 +21,9 @@ t.test('Class functions', async t => {
     c() {
       return 7;
     }
+    f(a,b,c,d,e) {
+      return a+b+c+d+e;
+    }
     met$h_od2 (  a , b   ) {
       return a + b;
     }
@@ -25,7 +32,39 @@ t.test('Class functions', async t => {
     }
   }
 
-  const facts = new TestClass();
-  t.same(await clues(facts, 'method1'), 26);
-  t.same(await clues(facts, 'method3'), 15);
+  class TestClass2 {
+    a() {
+      return 2;
+    }
+  }
+
+  const a = {
+    class: TestClass,
+    class2: TestClass2,
+    d: 11,
+    e: 42
+  };
+
+  const b = {
+    class: TestClass,
+    d: 12,
+    e: 60
+  };
+
+  t.test('Solving functions of an instance', async t => {
+    const facts = new TestClass();
+    t.same(await clues(facts, 'method1'), 26);
+    t.same(await clues(facts, 'method3'), 15);
+  });
+
+  t.test('Generating instance from a class', {autoend: true}, async t => {
+    t.test('with constructor', async t => {
+      t.same(await clues(a,'class.f'),75);
+      t.same(await clues(b,'class.f'),94);
+    });
+
+    t.test('without constructor', async t => {
+      t.same(await clues(a,'class2.a'),2);
+    }); 
+  });
 });
